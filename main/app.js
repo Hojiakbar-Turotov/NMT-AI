aeee949be600fa57b09c4c23c2073ca50a39f17c
Hojiakbar-Turotov <turatovhojiakbar@gmail.com>
2024-03-11 11:31:05 +0500

    Zanjirning bir qismi uchun Om qonuni

--- main/app.js
+++ main/app.js
@@ -38,6 +38,9 @@ function mavzularniSaralash() {
     if (mavzularBox[2].checked) {
         listNewMasala = listNewMasala.concat(elektr_qarshilik)
     }
+    if (mavzularBox[3].checked) {
+        listNewMasala = listNewMasala.concat(zanjirning_bir_qismi_uchun_om_qonuni)
+    }
     /* for (let i = 0; i < mavzularBox.length; i++) {
          if (mavzularBox[i].checked) {
              console.log(mavzular[i].innerText)
@@ -80,7 +83,7 @@ const tok_kuchi = [
     `Elektr lampochkadan ${randomNumber(80) / 100} A tok o‘tmoqda. Uning spirali ko‘ndalang kesim yuzasidan ${randomNumber(30)} minutda o‘tgan elektronlarning massasini aniqlang.`,
     `Manbaga ulangan iste’molchidan ${randomNumber(20)} mA tok o‘tib turibdi. Tok manbai ${randomNumber(120)} minut davomida zaryadni ko‘chirishda ${randomNumber(720)} J ish bajargan bo‘lsa, iste’molchi uchlariga qanday kuchlanish berilgan?`,
     `Elektr zanjirdagi lampochkadan ${randomNumber(40) / 100} A tok o‘tmoqda. Lampochka spirali orqali uch minutda qancha zaryad va nechta elektron o‘tishini hisoblang.`,
-    `12 V kuchlanishli akkumulyator avtomobilni yurgizishda generatorga ${randomNumber(50)} A tok bermoqda. Agar avtomobil dvigateli ${randomNumber(20)} s o‘tgach o‘t olsa, akkumulyator qanday ish bajargan?`,
+    `${randomNumber(12)} V kuchlanishli akkumulyator avtomobilni yurgizishda generatorga ${randomNumber(50)} A tok bermoqda. Agar avtomobil dvigateli ${randomNumber(20)} s o‘tgach o‘t olsa, akkumulyator qanday ish bajargan?`,
     `Elektr zanjiriga ulangan elektr lampochkadan ma’lum vaqt davomida ${randomNumber(25)} C zaryad o‘tib, tok manbai ${randomNumber(9) * 25} J ish bajardi. Lampochka spiraliga qanday kuchlanish berilgan?`,
 ]
 const elektr_qarshilik = [
@@ -89,6 +92,25 @@ const elektr_qarshilik = [
     `Uzunligi ${randomNumber(300) / 100} m, ko‘ndalang kesimining yuzasi ${randomNumber(5) / 10} mm^2 bo‘lgan simning qarshiligi ${randomNumber(240) / 100} Ω ga teng. Sim qanday moddadan tayyorlangan?`,
     `Bir xil moddadan tayyorlangan ikkita o‘tkazgich sim bor. Birinchi simning uzunligi ${randomNumber(5)} m, ko‘ndalang kesimining yuzasi ${randomNumber(5) / 10} mm^2 ikkinchi simning uzunligi ${randomNumber(5) / 10} m, ko‘ndalang kesimining yuzasi ${randomNumber(50) / 10} mm^2. Qaysi simning qarshiligi katta va necha marta katta?`,
     `Ko‘ndalang kesimining yuzasi ${randomNumber(5) / 10} mm^2  bo‘lgan ${randomNumber(5)} Ω qarshilikli spiral tayyorlash uchun qanday uzunlikda nikelin sim kerak bo‘ladi?`,
+]
+
+const zanjirning_bir_qismi_uchun_om_qonuni = [
+    `Uzunligi ${randomNumber(8)} m, ko‘ndalang kesimining yuzi ${randomNumber(5) / 10} mm^2 bo‘lgan nikelin simning uchlaridagi kuchlanish ${randomNumber(4)} V ga teng. Shu elektr zanjir orqali o‘tayotgan tok kuchini aniqlang.`,
+    `Elektr zanjirga ulangan o‘tkazgichning qarshiligi ${randomNumber(100)} Ω. Qarshilik uchlaridagi kuchlanish ${randomNumber(10)} V bo‘lsa, undan qanday tok o‘tadi?`,
+    `Qarshiligi ${randomNumber(125)} Ω bo‘lgan o‘tkazgich orqali ${randomNumber(8) / 10} A tok o‘tkazish uchun o‘tkazgich uchlariga qanday kuchlanish qo‘yish kerak?`,
+    `Elektr zanjiridagi iste’molchiga ${randomNumber(6)} V kuchlanish berilganda, undagi tok kuchi ${randomNumber(20) / 100} A ga teng bo‘ldi. Shu iste’molchida tok kuchi ${randomNumber(30) / 100} A ga yetishi uchun unga qanday kuchlanish berish kerak?`,
+    `Uzunligi ${randomNumber(20)} m, ko‘ndalang kesimi ${randomNumber(40) / 100} mm^2 bo‘lgan alyuminiy simning uchlariga ${randomNumber(7)}simning uzunligi qanday bo‘lgan?`,
+    `Ko‘ndalang kesim yuzi ${randomNumber(20) / 100} mm^2 bo‘lgan nikelin o‘tkazgich uchlariga ${randomNumber(45) / 100} V kuchlanish berilganda undan ${randomNumber(300)} mA tok o‘tdi. O‘tkazgich uzunligi qanday bo‘lgan?`,
+    `Uzunligi ${randomNumber(20)} m va ko‘ndalang kesim yuzi ${randomNumber(10)} mm^2 bo‘lgan nixrom o‘tkazgich uchlariga ${randomNumber(44)} mV kuchlanish berilganda undan qanday tok o‘tadi?`,
+    `Cho‘g‘lanma lampochka spiralining uzunligi ${randomNumber(10)} cm va ko‘ndalang kesim yuzi ${randomNumber(60) / 10000} mm^2 bo‘lgan volframdan yasalgan. Lampochkaga ketma-ket ulangan ampermetr ${randomNumber(24) / 100} A tok kuchini ko‘rsatmoqda. Lampochka uchlaridagi kuchlanishni aniqlang.`,
+    `O‘tkazgich uchlariga ${randomNumber(6)} V kuchlanish berilganda undan ${randomNumber(5)} s da ${randomNumber(20)} C zaryad o‘tdi. O‘tkazgich qarshiligi qanchaga teng?`,
+    `Uzunligi ${randomNumber(12)} m va ko‘ndalang kesim yuzi ${randomNumber(60) / 100} mm^2 bo‘lgan nixrom o‘tkazgich uchlariga ${randomNumber(44) / 100} V kuchlanish berilganda undan qancha tok o‘tadi?`,
+    `Uzunligi ${randomNumber(5)} m, ko‘ndalang kesim yuzi ${randomNumber(50) / 100} mm^2 bo‘lgan simning qarshiligi ${randomNumber(4)} Ω ga teng. Sim qanday moddadan tayyorlangan?`,
+    `Qarshiligi ${randomNumber(10)} Ω bo‘lgan o‘tkazgich uchlariga ${randomNumber(250) / 100} V kuchlanish berilgan. O‘tkazgich ko‘ndalang kesim yuzidan ${randomNumber(8)} s da qancha elektron o‘tadi?`,
+    `Ko‘ndalang kesim yuzi ${randomNumber(10) / 100} mm^2 bo‘lgan nixromdan elektr plitasining qizdirgichi yasalgan. Uning uchlariga ${randomNumber(220)} V kuchlanish berilganda undan ${randomNumber(4)} A tok o‘tdi. Qizdirgichga qanday uzunlikdagi sim olingan?`,
+    `Qarshiligi ${randomNumber(170) / 100} Ω bo‘lgan mis simda ${randomNumber(50) / 100} A tok hosil qilish uchun shu simning uchlariga qancha kuchlanish qo‘yish kerak? Simning ko‘ndalang kesim yuzi ${randomNumber(50) / 100} mm^2 bo‘lsa, uning uzunligi qanday bo‘ladi?`,
+    `Uzunligi ${randomNumber(100)} m, ko‘ndalang kesim yuzi ${randomNumber(50) / 100} mm^2 bo‘lgan alyuminiy simning uchlaridagi ${randomNumber(7)} V kuchlanish berilganda shu simdan o‘tayotgan tok kuchi qancha bo‘ladi?`,
+    `Akkumulyatorga ulangan voltmetr ${randomNumber(12)} V ko‘rsatadi. Agar akkumulyator qisqichlaridan biri yerga ulansa, voltmetr ko‘rsatishida qanday o‘zgarish bo‘ladi?`,
 ]
 
 // tasodifiy raqamlarni chiqarish
